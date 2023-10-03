24899b9b50d17c9abb1d676130252342b89e5e67
billygotclout <williamsoluwademilade@gmail.com>
2023-10-03 07:38:38 +0100

    setup testing using jest

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/userRoute"));
+app.use("/api/product", require("./routes/productRoute"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+app.route("/").get((req, res) => {
+  res.send("APP is running");
+});
+module.exports = app;
